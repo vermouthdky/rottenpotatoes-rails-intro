701592fa9f6501b3c9c5bb3b40c8877f0dd7e8b6
EC2 Default User <ec2-user@ip-172-31-31-61.us-east-2.compute.internal>
2021-02-19 13:19:07 +0000

    finish part3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,34 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+
+    sort = params[:sort] || session[:sort]
+    if sort == "movies_title"
+      order = {title: :asc}
+      @title_class = 'bg-warning'
+      @release_date_class = ''
+    elsif sort == "release_date"
+      order = {release_date: :asc}
+      @release_date_class = 'bg-warning'
+      @title_class = ''
+    end
+    
+    @all_ratings = Movie.all_ratings
+    
+    @ratings_to_show = params[:ratings] || session[:ratings] || Hash[@all_ratings.map {|rating| [rating, 1]}]
+
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @ratings_to_show
+      # specifically, when resetting ratings = {}, session == all ratings
+      if !params[:ratings] and !params[:sort]
+        session[:ratings] = Hash[@all_ratings.map {|rating| [rating, 1]}]
+      end
+      redirect_to sort: session[:sort], ratings: session[:ratings] and return
+    end
+    
+    @movies = Movie.with_ratings(@ratings_to_show).order(order)
+    
   end
 
   def new
